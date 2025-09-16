659c5e0ad883de37e3fca93fc0b0324424feee2e
Shishir <shishir16subedi@gmail.com>
2025-09-16 21:41:24 +0545

    Feat(project): added entity

--- src/project/entities/project.entity.ts
+++ src/project/entities/project.entity.ts
@@ -1,1 +1,43 @@
-export class Project {}
+// src/projects/entities/project.entity.ts
+
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    ManyToOne,
+    CreateDateColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+// import { Team } from 'src/teams/entities/team.entity';
+import { ProjectStatus } from 'src/common/enums/project-status.enum';
+
+@Entity('projects')
+export class Project {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({ length: 100 })
+    name: string;
+
+    @Column({ type: 'text', nullable: true })
+    description?: string;
+
+    @Column({
+        type: 'enum',
+        enum: ProjectStatus,
+        default: ProjectStatus.NOT_STARTED,
+    })
+    status: ProjectStatus;
+
+    @Column({ type: 'timestamptz', nullable: true })
+    deadline?: Date;
+
+    // @ManyToOne(() => Team, (team) => team.projects, { onDelete: 'CASCADE' })
+    // team: Team;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
